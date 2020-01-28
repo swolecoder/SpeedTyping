9d7febb4a9d1d823e2913a2d7463f87012d145a8
Ashish Ranjan <swolecoder@gmail.com>
2020-01-28 12:55:36 -0500

    adding stuff

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import "./App.css";
 function App() {
   const [text, setText] = useState("");
   const [time, setTimeRemaning] = useState(5);
+  const [timerRunning, setTimerRunning] = useState(false);
 
   useEffect(() => {
-    if (time > 0) {
+    if (timerRunning && time > 0) {
       setTimeout(() => {
         setTimeRemaning(prevTime => prevTime - 1);
       }, 1000);
     }
-  }, [time]);
+  }, [time, timerRunning]);
 
   function handleChange(e) {
     const { value } = e.target;
@@ -26,12 +27,16 @@ function App() {
     return wordsArr.length;
   }
 
+  function startTimer() {
+    setTimerRunning(previosState => true);
+  }
+
   return (
     <div>
       <h1>How fast do you type?</h1>
       <textarea onChange={handleChange} value={text} />
       <h4>Time remaining: {time}</h4>
-      <button onClick={() => calculateWordCount(text)}>Start</button>
+      <button onClick={() => startTimer()}>Start</button>
       <h1>Word count: ???</h1>
     </div>
   );
